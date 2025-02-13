4926f7618b83f223e8d7037d5afa239ca17137fe
katevij <katevij10@gmail.com>
2025-02-13 00:38:44 +0000

    started pet class

---
+++ pet.py
@@ -0,0 +1,14 @@
+class pet:
+    def __init__(self):
+        self.hunger=80
+        self.happiness=60
+        self.currency=0
+        self.skills
+        self.mood
+        self.age
+
+#0/baby, toddler, child, teen, adult
+#outside, hydrate, eat, sleep, productivity(for study and such), connecting with yourself
+#daily positive quote ^)
+#onthevergeofdeath, happy, nervous, sad, mad, stressed, excited, comfortable, relentless, chill, thriving
+#hunger, happiness, mood, age, currency, skills
