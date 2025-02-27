bd4f2657bee8e83ae9bf0967a8cbe614fbbe57ab
Lynn-Chian Chen <127175081+chenlynnc@users.noreply.github.com>
2025-02-27 02:20:06 +0000

    worked on level up, play

--- pet.py
+++ pet.py
@@ -1,20 +1,23 @@
 class Pet:
     MOOD = ['sad','mad','happy','excited','chill','stressed','relaxed','hungry','onthevergeofdeath']
-    
+    # Add list of ages: baby, toddler, child, teen, adult
+    # Static variable: max xp possible
+
     def __init__(self, name):
         self.name = name
         self.hunger = 80
         self.happiness = 60
         self.currency = 0
         self.skills = {
-            "Sleeping" : 0,
+            "Resting" : 0,
             "Productivity" : 0,
             "Outside" : 0,
             "Hydrate" : 0,
             "Connecting" : 0
         }
         self.mood = "chill"
-        self.xp = 0
+        self.xp = 0  # Max 50 for now?
+        self.age = "baby"
 
     def printStats(self):
         print(f"Name: {self.name}")
@@ -30,18 +33,46 @@ class Pet:
         return
         
     def rest(self):
-        # make "sleeping" skill increase exp
+        # make "resting" skill increase exp
         return
         
     def hydrate(self):
+        # make "hydrate" skill increase exp
+        return
+    
+    def beProductive(self):
+        # make "Productivity" skill increase exp
         return
         
-    def play(self):
-        #Play == hobbies?
+    def goOutside(self):
+        # Play == hobbies?
+        # max 50 for now
+        self.xp += 10
+        
+        if self.xp > 50:
+            self.xp -= 50 # When xp reaches 50, subtract past xp -> go to a new level.
+            self.levelUp(); 
+        
+        return
+    
+    def connectWith(self):
+        # make "COnnecting" skill increase exp
+        return
+    
+    def levelUp(self):
+        print(f"Congrats, you leveled up to: {self.age}")
+        self.age += 1 #0/baby, 1-toddler, 2-child, 3-teen, 4-adult
+        return
+    
+    def addXp(self, experience):
+        # 1. experience gets added to self.xp
+        # 2. check if xp has surpassed max xp limit (aka xp should never surpass 50)
+        # 3. if so, level up and set xp backward
         return
 
 #age and XP are the same thing, how do you want to do it?
 #0/baby, toddler, child, teen, adult
+#  0       1.       2.     3.    4
 #outside, hydrate, eat, sleep, productivity(for study and such), connecting with yourself
 #daily positive quote ^)
 #hunger, happiness, mood, age, currency, skills
