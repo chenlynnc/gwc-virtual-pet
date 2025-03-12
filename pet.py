18cfc479040699a4f5c076e2dc5c0a3d965e2d7f
Lynn-Chian Chen <127175081+chenlynnc@users.noreply.github.com>
2025-03-12 23:44:44 +0000

    implement rest(), beProductive(), goOutside(), finished addXp()

--- pet.py
+++ pet.py
@@ -2,6 +2,7 @@ class Pet:
     MOOD = ['sad','mad','happy','excited','chill','stressed','relaxed','hungry','onthevergeofdeath']
     # Add list of ages: baby, toddler, child, teen, adult
     # Static variable: max xp possible
+    # Add dict of skill levels, OR make the current skills dict values an int pair [exp, level]
 
     def __init__(self, name):
         self.name = name
@@ -41,29 +42,48 @@ class Pet:
     
     '''
     using rest() will increase the "resting" skill exp by some amount.
-    should it increase both "normal" exp AND skill exp?
     should we print something?
     '''
     def rest(self):
-        # rest the pet to restore energy and to add XP
-        self.addXP()
-        #HOW MUCH SLEEP IS TOO MUCH SLEEP DO THEY DIE?
-        return
+        # Add XP for resting?
+        self.addXP(10, "Rest") # we can change the points if we would like, bc idk how much sleep
+        
+        #should we increase energy level by like 20 and max at 100?
+        if self.energy_level < 100:
+            self.energy_level += 20
+        if self.energy_level > 100:
+            self.energy_level = 100 #how do we ensure that this won't exceed 100? 
+            
+        print(f"{self.name}'s energy level is now {self.energy_level}%.")
+
+        #We can make a fun little message about resting 
+        print(f"Time to rest and rechage, {self.name}!!! Zzzzz...")
+
+        return self.energy_level
+    
+    #Hopefully it will run like this 
+    #my_pet = Pet("Buddy")
+    #my_pet.rest()
+    # Output:
+    #buddy earned 20 XP for rest! Total XP : 20
+    #Buddy's energy level is now 70%.
+    # Time to rest and recharge, buddy! Zzzzz....
 
     '''
-    using hydrate() will increase the "hydrate" skill exp by some amount.
-    should it increase both "normal" exp AND skill exp?
-    should we print something?
+    using hydrate() will:
+     - increase the "hydrate" skill exp by 5
+     - increase hydration_level by 5
+     - print a lil quote
     ''' 
     def hydrate(self):
         # when you hydrate your pet, adds XP, and will update the level
         self.addXp(5, "Hydrate")
 
-        # increase hydration level by 5? (max out 200? what #)
-        if self.hydration_level < 200:
+        # increase hydration level by 5? (max out 100? what #)
+        if self.hydration_level < 100:
                 self.hydration_level += 5
-                if self.hydration_level > 200:
-                    self.hydration_level = 200
+                if self.hydration_level > 100:
+                    self.hydration_level = 100
         print(f"{self.name}'s hydration level is now {self.hydration_level}%.")
 
 
@@ -86,17 +106,15 @@ class Pet:
     
     '''
     using beProductive() will increase the "Productivity" skill exp by some amount.
-    should it increase both "normal" exp AND skill exp?
-    should we print something?
     '''
     def beProductive(self):
         # make "Productivity" skill increase exp
+        self.addXp(5, "Productivity")
+
         return
     
     '''
     using goOutside() will increase the "Outside" skill exp by some amount.
-    should it increase both "normal" exp AND skill exp?
-    should we print something?
     '''
     def goOutside(self):
         # Play == hobbies?
@@ -104,16 +122,11 @@ class Pet:
         
         #Call the method addxp #self, experience, skill = None
         self.addXp(5, "Outside") #Adds to the outside skill. Am i doing this right? 
-
-        #Print statement:
-        print("Great job! You went outside and gained experience points!")
         
         return
     
     '''
     using connectWith() will increase the "Connecting" skill exp by some amount.
-    should it increase both "normal" exp AND skill exp?
-    should we print something?
     '''
     def connectWith(self):
         # make "COnnecting" skill increase exp
@@ -121,12 +134,20 @@ class Pet:
     
     '''
     using levelUp() will increase the pet's age by 1.
+        only happens IF all skills are at the same level already.
+        increase happiness
     prevent age from surpassing 4 (adult)?
-    should it print something?
     '''
     def levelUp(self):
         print(f"Congrats, you leveled up to: {self.age}")
         self.age += 1 #0/baby, 1-toddler, 2-child, 3-teen, 4-adult
+        return
+    
+    '''
+    Levels up skills.
+    Increase happiness
+    '''
+    def skillLevelUp(self):
         return
     
     '''
@@ -141,12 +162,17 @@ class Pet:
         1. experience gets added
         2. check if xp has surpassed max xp limit (aka xp should never surpass 50)
         3. if so, level up and set xp backward
-    should we print something?
+    
+    Today:
+        1. Print "{name} earned {experience} XP for the {skill} skill!" if a skill was increased
+            Else, just print ""{name} earned {experience} XP!"
+        2. Should skills have levels? If so, we need to handle the leveling up.
     '''
     def addXp(self, experience, skill = None):
         
         if skill == None: # addXp(5)
             self.xp += experience # 1. experience gets added
+            print(f"{self.name} earned {experience} XP!")
             
             if self.xp > 50: # 2. check if xp has surpassed max xp limit (aka xp should never surpass 50)
                 self.xp -= 50 # When xp reaches 50, subtract past xp -> go to a new level.
@@ -154,21 +180,24 @@ class Pet:
         
         elif skill == "Resting": #Increasing resting xp
             self.skills["Resting"] += experience
+            print(f"{self.name} earned {experience} XP for the {skill} skill!")
         
         elif skill == "Productivity":
             self.skills["Productivity"] += experience #Increasing productivity xp
-       
+            print(f"{self.name} earned {experience} XP for the {skill} skill!")
+            
         elif skill == "Outside":
             self.skills["Outside"] += experience #Increasing Outside xp
-       
+            print(f"{self.name} earned {experience} XP for the {skill} skill!")
+        
         elif skill == "Hydrate":
-            self.skills["Hydrate"] += experience
+            self.skills["Hydrate"] += experience # increase hydrate exp
             # print: buddy earned 5 XP for Hydrate! Total XP: 5
-            # increase hydrate exp
+            print(f"{self.name} earned {experience} XP for the {skill} skill!")    
         
         elif skill == "Connecting":
             self.skills["Connecting"] += experience #Increasing connecting xp
-
+            print(f"{self.name} earned {experience} XP for the {skill} skill!")
         return
 
 #age and XP are the same thing, how do you want to do it?
